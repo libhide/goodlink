de2fb8d89440e56f3aab6babf9a48d6cc8831690
Ratik Sharma <Ratik96@gmail.com>
2019-09-08 19:56:50 +0530

    Build logic to determine a book page or not

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('kitten kitten kitten go go go!');
+const isBookPage = isbnQuery => {
+  return isbnQuery != undefined && isbnQuery.innerText.includes('ISBN');
+};
+
+const isbnQuery = document.querySelectorAll('.content li')[3];
+
+if (isBookPage()) {
+  console.log('book page');
+} else {
+  console.log('nooope');
+}
