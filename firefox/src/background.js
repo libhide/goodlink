931427b74a973fa6bfe70a3f569caf774c855d22
Ratik Sharma <Ratik96@gmail.com>
2019-10-02 11:55:26 +0530

    Add and tweak extension for firefox

--- firefox/src/background.js
+++ firefox/src/background.js
@@ -9,23 +9,23 @@ const openGRListener = async (request, sender, sendResponse) => {
   if (request.message === MSG_OPEN_GR) {
     const isbn = request.data;
     const bookUrl = await getBookUrl(developerKey, isbn);
-    chrome.tabs.create({ url: bookUrl });
+    browser.tabs.create({ url: bookUrl });
   }
 };
 
 const bookPageNotificationListener = (request, sender, sendResponse) => {
   if (request.message === MSG_BOOK_PAGE_NOTIFY) {
-    chrome.browserAction.onClicked.addListener(tab => {
+    browser.browserAction.onClicked.addListener(tab => {
       const message = new Message(MSG_EXT_CLICKED);
-      chrome.tabs.sendMessage(tab.id, message);
+      browser.tabs.sendMessage(tab.id, message);
     });
 
-    chrome.browserAction.setIcon({
+    browser.browserAction.setIcon({
       path: request.data,
       tabId: sender.tab.id
     });
   }
 };
 
-chrome.runtime.onMessage.addListener(bookPageNotificationListener);
-chrome.runtime.onMessage.addListener(openGRListener);
+browser.runtime.onMessage.addListener(bookPageNotificationListener);
+browser.runtime.onMessage.addListener(openGRListener);
