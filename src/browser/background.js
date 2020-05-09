a958bcda966a28fa643f57051fc644695dab86ca
Ratik Sharma <Ratik96@gmail.com>
2020-05-09 21:38:54 +0530

    fix multiple tabs opening bug for firefox

--- src/browser/background.js
+++ src/browser/background.js
@@ -9,16 +9,21 @@ const openGRListener = async (request, sender, sendResponse) => {
   if (request.message === MSG_OPEN_GR) {
     const isbn = request.data;
     const bookUrl = await getBookUrl(developerKey, isbn);
+    console.log(bookUrl);
     browser.tabs.create({ url: bookUrl });
   }
 };
 
+const handleExtensionClick = (tab) => {
+  const message = new Message(MSG_EXT_CLICKED);
+  browser.tabs.sendMessage(tab.id, message);
+};
+
 const bookPageNotificationListener = (request, sender, sendResponse) => {
   if (request.message === MSG_BOOK_PAGE_NOTIFY) {
-    browser.browserAction.onClicked.addListener(tab => {
-      const message = new Message(MSG_EXT_CLICKED);
-      browser.tabs.sendMessage(tab.id, message);
-    });
+    if (!browser.browserAction.onClicked.hasListener(handleExtensionClick)) {
+      browser.browserAction.onClicked.addListener(handleExtensionClick);
+    }
 
     browser.browserAction.setIcon({
       path: request.data,
@@ -27,5 +32,5 @@ const bookPageNotificationListener = (request, sender, sendResponse) => {
   }
 };
 
-browser.runtime.onMessage.addListener(bookPageNotificationListener);
 browser.runtime.onMessage.addListener(openGRListener);
+browser.runtime.onMessage.addListener(bookPageNotificationListener);
