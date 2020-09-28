d4dcf53403ceee3947d207ff731cad4127bf4069
Ratik Sharma <Ratik96@gmail.com>
2020-09-28 17:41:10 +0530

    chore(*): bump deps

--- webpack.utils.js
+++ webpack.utils.js
@@ -1,35 +1,37 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const getOutput = (browserDir, outputDir = 'dev') => {
+const getOutput = (browserDir, outputDir = "dev") => {
   return {
     path: path.resolve(__dirname, `${outputDir}/${browserDir}`),
-    filename: '[name].js'
+    filename: "[name].js",
   };
 };
 
-const getEntry = (sourceDir = 'src') => {
+const getEntry = (sourceDir = "src") => {
   return {
     content: path.resolve(__dirname, `${sourceDir}/content.js`),
-    background: path.resolve(__dirname, `${sourceDir}/background.js`)
+    background: path.resolve(__dirname, `${sourceDir}/background.js`),
   };
 };
 
-const getCopyPlugins = (browserDir, outputDir = 'dev', sourceDir = 'src') => [
-  new CopyWebpackPlugin([
-    {
-      from: path.resolve(__dirname, './icons'),
-      to: path.resolve(__dirname, `${outputDir}/${browserDir}/icons`)
-    },
-    {
-      from: path.resolve(__dirname, './manifest.json'),
-      to: path.resolve(__dirname, `${outputDir}/${browserDir}/manifest.json`)
-    }
-  ])
+const getCopyPlugins = (browserDir, outputDir = "dev", sourceDir = "src") => [
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, "./icons"),
+        to: path.resolve(__dirname, `${outputDir}/${browserDir}/icons`),
+      },
+      {
+        from: path.resolve(__dirname, "./manifest.json"),
+        to: path.resolve(__dirname, `${outputDir}/${browserDir}/manifest.json`),
+      },
+    ],
+  }),
 ];
 
 module.exports = {
   getOutput,
   getCopyPlugins,
-  getEntry
+  getEntry,
 };
